9107d4ee1ebb59afac645772c626d939f8cac187
Marin Bibot <Marin.bibot@hotmail.be>
2020-05-09 13:59:46 +0200

    Updates

---
+++ precache-manifest.4cf63bd3d00a48fb3095b70c2ed495f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49867290398f984d53addac43987f3d2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4bc5a4cbefa88311077f",
+    "url": "/portfolio/static/css/main.cda236ba.chunk.css"
+  },
+  {
+    "revision": "980bb0df26582294be7c",
+    "url": "/portfolio/static/js/2.4af44fa9.chunk.js"
+  },
+  {
+    "revision": "e95b3ebaec68caad50c330e63af5be13",
+    "url": "/portfolio/static/js/2.4af44fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc5a4cbefa88311077f",
+    "url": "/portfolio/static/js/main.7f79a44f.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
